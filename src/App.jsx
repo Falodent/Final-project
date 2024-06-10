e519d7ae71d5db2116dc12c21ece1be9fa46ad73
Falodent <falodave11@gmail.com>
2024-06-10 20:06:32 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div>This is the Home Page of our Application</div>
+    <div>This is the Page of our Application that we call Home</div>
   )
 }
 export default App
